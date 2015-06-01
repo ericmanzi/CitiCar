e39c41b9d5599cef07537bbd6f17d64a16269adb
Eric Manzi <ermanzi@mit.edu>
2015-06-01 17:26:09 -0400

    get user's location

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
 	testObject.save({foo: "bar"}).then(function(object) {
 		alert("yay! it worked");
 	}); 
+
+
+	//GET USER'S LOCATION
+	<script>
+	var x = document.getElementById("demo");
+
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } else { 
+	        x.innerHTML = "Geolocation is not supported by this browser.";
+	    }
+	}
+
+	function showPosition(position) {
+	    x.innerHTML = "Latitude: " + position.coords.latitude + 
+	    "<br>Longitude: " + position.coords.longitude;	
+	}
+	</script>
+
 });
