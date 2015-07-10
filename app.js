6453f2a461ea5d197d1beece25743c11e70898a1
Eric Manzi <emanzi@akamai.com>
2015-07-09 21:00:44 -0400

    fbUser

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
 });
 
 $(document).on("click", ".login-facebook-btn", function() {
-	Parse.FacebookUtils.logIn(["public_profile","email"], {
+	Parse.FacebookUtils.logIn(null, {
 		success: function(user) {
 			if (!user.existed()) {
 				alert("User signed up and logged in through Facebook!");
