6df87b7889527756ab726c4c856f93920ffe3fca
Eric Manzi <emanzi@akamai.com>
2015-07-14 18:42:31 -0400

    remove ?123

--- profile/profile.js
+++ profile/profile.js
@@ -26,7 +26,7 @@ app.controller('ProfileCtrl', function() {
     self.return_time = 0.0;
 
     if (self.user===undefined || self.user===null) {
-        window.location = "../login.html?123";
+        window.location = "../login.html";
     } else {
         self.currentView = "reserve";
     }
