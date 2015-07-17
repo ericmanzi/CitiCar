f4a412a9f8873ef2814d73af95465e93d7fc563b
ermanzi <ermanzi@mit.edu>
2015-07-16 22:03:38 -0400

    basdfasd

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ $(document).ready(function(){
 $(document).on("click", ".login-facebook-btn", function() {
 	Parse.FacebookUtils.logIn(null, {
 		success: function(user) {
+		    console.log((typeof user));
 			if (!user.existed()) {
 				alert("User signed up and logged in through Facebook!");
 			} else {
