8ccc6e969218981d4855ece6ebfb0a31741d50a3
Eric Manzi <emanzi@akamai.com>
2015-07-29 10:55:31 -0400

    parse stuff

--- app.js
+++ app.js
@@ -1,65 +1,30 @@
 var lat=0.0;
 var long=0.0;
 
-//ALLOW CROSS ORIGIN REQUESTS
-//jQuery.support.cors = true;
-//$.ajaxTransport("+*", function( options, originalOptions, jqXHR ) {
-//    if(jQuery.browser.msie && window.XDomainRequest) {
-//        var xdr;
-//        return {
-//            send: function( headers, completeCallback ) {
-//                // Use Microsoft XDR
-//                xdr = new XDomainRequest();
-//                xdr.open("get", options.url);
-//                xdr.onload = function() {
-//                    if(this.contentType.match(/\/xml/)){
-//                        var dom = new ActiveXObject("Microsoft.XMLDOM");
-//                        dom.async = false;
-//                        dom.loadXML(this.responseText);
-//                        completeCallback(200, "success", [dom]);
-//                    }else{
-//                        completeCallback(200, "success", [this.responseText]);
-//                    }
-//                };
-//                xdr.ontimeout = function(){
-//                    completeCallback(408, "error", ["The request timed out."]);
-//                };
-//                xdr.onerror = function(){
-//                    completeCallback(404, "error", ["The requested resource could not be found."]);
-//                };
-//                xdr.send();
-//            },
-//            abort: function() {
-//                if(xdr)xdr.abort();
-//            }
-//        };
-//    }
-//});
-
 //GET USER'S LOCATION
 function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
-	} else {
-		console.log("Geolocation is not supported by this browser.");
-	}
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
 }
 
 function showPosition(position) {
     lat = position.coords.latitude;
     long = position.coords.longitude;
-	console.log("Latitude: " + lat + "Longitude: " + long);
+    console.log("Latitude: " + lat + "Longitude: " + long);
 }
 
 function setupFacebook() {
-	window.fbAsyncInit = function() {
-		Parse.FacebookUtils.init({
-			appId      : '1620498488165422',
-			xfbml      : true,
-			status      : true,
-			cookie      : true,
-			version    : 'v2.3'
-		});
+    window.fbAsyncInit = function() {
+        Parse.FacebookUtils.init({
+            appId      : '1620498488165422',
+            xfbml      : true,
+            status      : true,
+            cookie      : true,
+            version    : 'v2.3'
+        });
 
         // Additional init code here
         FB.getLoginStatus(function(response) {
@@ -71,7 +36,7 @@ function setupFacebook() {
 //                alert("User logged in and connected. UserID: "+uid+", Access token: "+accessToken);
 
                 var apiUrl = "https://graph.facebook.com/v2.3/"+
-                            uid+"?access_token="+accessToken;
+                    uid+"?access_token="+accessToken;
 //              alert("getting "+apiUrl);
                 $.getJSON( apiUrl, function( data ) {
                     var fbUser = {};
@@ -84,36 +49,36 @@ function setupFacebook() {
 
             }
         });
-	};
+    };
 
-	(function(d, s, id){
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {return;}
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 }
 
 $(document).ready(function(){
-	Parse.initialize("MJ7veguTqdNXV8bF0x5IB6fAItT3gK22B5mrtzxD", "LrbtNhaspWUt86vml7TFS6gn3XSsezNjr8NLst4p");
+    Parse.initialize("MJ7veguTqdNXV8bF0x5IB6fAItT3gK22B5mrtzxD", "LrbtNhaspWUt86vml7TFS6gn3XSsezNjr8NLst4p");
 
-	getLocation();
+    getLocation();
 
-	setupFacebook();
+    setupFacebook();
 
 });
 
 $(document).on("click", ".login-facebook-btn", function() {
-	Parse.FacebookUtils.logIn(null, {
-		success: function(user) {
-			if (!user.existed()) {
+    Parse.FacebookUtils.logIn(null, {
+        success: function(user) {
+            if (!user.existed()) {
 //				alert("User signed up and logged in through Facebook!");
-			} else {
+            } else {
 //				alert("User logged in through Facebook! "
 //                    +JSON.stringify(user));
-			}
+            }
 
             var userString = JSON.stringify(user);
 
@@ -149,31 +114,31 @@ $(document).on("click", ".login-facebook-btn", function() {
                 window.location = './profile/profile.html';
             }, 'jsonp');
 
-		},
-		error: function(user, error) {
-			alert(JSON.stringify(error));
-		}
-	});
+        },
+        error: function(user, error) {
+            alert(JSON.stringify(error));
+        }
+    });
 });
 
 $(document).on("click", "#login-btn", function() {
-	var email = $("#email").val();
-	var pass = $("#password").val();
-	var username = email.split("@")[0];
+    var email = $("#email").val();
+    var pass = $("#password").val();
+    var username = email.split("@")[0];
 
-	Parse.User.logIn(username, pass, {
-		success: function(user) {
-			// Do stuff after successful login.
+    Parse.User.logIn(username, pass, {
+        success: function(user) {
+            // Do stuff after successful login.
             user.set("latitude", lat);
             user.set("longitude", long);
             sessionStorage.setItem("currentUser", JSON.stringify(user));
-			window.location = './profile/profile.html';
-		},
-		error: function(user, error) {
-			// The login failed. Check error to see why.
-			alert("The email/password combination you entered does not belong to any account.");
-		}
-	});
+            window.location = './profile/profile.html';
+        },
+        error: function(user, error) {
+            // The login failed. Check error to see why.
+            alert("The email/password combination you entered does not belong to any account.");
+        }
+    });
 });
 
 $(document).on('click', '#create_account', function() {
@@ -193,7 +158,6 @@ $(document).on('click', '#create_account', function() {
     user.set("full_name", first+" "+last);
     user.set("latitude", lat);
     user.set("longitude", long);
-
 
     if (pass===confirm_pass) {
         user.signUp(null, {
